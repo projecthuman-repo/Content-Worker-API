4537b3bd3ee40b7f8a4ad9d6b29bd10bab8c4f8f
Zia Syed <ziasyed2000@gmail.com>
2023-02-22 20:09:13 -0500

    setup route for text moderation + module import

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from typing import Union
 
 from fastapi import FastAPI
+from profanity_check import predict
 
 app = FastAPI()
 
 # Base route returning an object of hello world
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "PHC: Content Moderation Server - Version 0.1"
+
+# Base route returning an object of hello world
+@app.post("/moderate/text")
+def read_root():
+    return ""
 
 # Route to manage queries within the request URL
 @app.get("/items/{item_id}")
