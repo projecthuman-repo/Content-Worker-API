e7665f5ac52ffb413810baa32210da6bd0646d6a
Zia Syed <ziasyed2000@gmail.com>
2023-02-22 20:32:45 -0500

    working text moderation route

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from typing import Union
 
 from fastapi import FastAPI
 from profanity_check import predict
+from pydantic import BaseModel
+
+# Temporary schema
+# Need to work on the instant text verification request schema 
+# (may also stay the same)
+class InstantTextVerification(BaseModel):
+    text: str
 
 app = FastAPI()
 
@@ -10,10 +17,19 @@ app = FastAPI()
 def read_root():
     return "PHC: Content Moderation Server - Version 0.1"
 
-# Base route returning an object of hello world
+# Receives a POST request with a text payload, performs a moderation 
+# check on the text using a machine learning model (via the predict() function), 
+# and returns a pass/fail result based on the model's prediction.
 @app.post("/moderate/text")
-def read_root():
-    return ""
+def read_root(request: InstantTextVerification):
+    stringToCheck = request.text
+    result = predict([stringToCheck])
+
+    # Simple check to see if the check passed or failed
+    if (result == [1]):
+        return {"result": "fail"}
+    else:
+        return {"result": "pass"}
 
 # Route to manage queries within the request URL
 @app.get("/items/{item_id}")
