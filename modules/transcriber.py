76d8da29b5c8433760527def480803022220536c
Zia Syed <ziasyed2000@gmail.com>
2023-03-20 18:28:47 -0400

    removed global variable in favor of argument for filename

--- modules/transcriber.py
+++ modules/transcriber.py
@@ -3,14 +3,11 @@ import speech_recognition as sr
 # TEST SETUP for the audio transcriber.
 # Will need additional setup to work with incoming requests/convert/download audio files appropriately.
 
-# Variable containing the filename to be transcribed
-AUDIO_FILE = "transcript.wav"
-
-def transcribeAudio():
+def transcribeAudio(audioFileName):
     r = sr.Recognizer()
     
     # use the audio file as the audio source
-    with sr.AudioFile(AUDIO_FILE) as source:
+    with sr.AudioFile(audioFileName) as source:
         # read the entire audio file
         audio = r.record(source)
         transcribed_text = r.recognize_google(audio)
