a8c51c384b42a5bd10fa29b7906fdca9c2a7c57d
Zia Syed <ziasyed2000@gmail.com>
2023-03-25 23:09:56 -0400

    typeFinder code cleanup

--- modules/typeFinder.py
+++ modules/typeFinder.py
@@ -5,15 +5,17 @@ import magic
 # Figures out the extension or the file type
 
 # Based on the filename passed as argument, this method checks whether the file 
-# currently has a type, if so print that, otherwise we use the magic library to 
-# detect what type of file it is.
+# currently has a type, if so return that, otherwise we use the magic library to 
+# detect the correct file type to return.
 def findFileType(filename):
+    file_type: str
+
     # check if file has an extension
     if '.' not in filename:
         # determine file type based on contents
         file_type = magic.from_file(filename)
-        print(file_type)
     else:
         # get the file extension
-        file_extension = os.path.splitext(filename)[1]
-        print(file_extension)
+        file_type = os.path.splitext(filename)[1]
+    
+    return file_type
